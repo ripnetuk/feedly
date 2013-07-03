9402ac6e4106c345398b8ac90e9707ae3e7bafd2
George Styles <george@ripnet.co.uk>
2013-07-03 15:18:38 +0100

    added feedly

---
+++ src/feedly.ripnet.co.uk/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="feedly.ripnet.co.uk.MvcApplication" Language="C#" %>
---
+++ src/feedly.ripnet.co.uk/Models/HomeModels.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace feedly.ripnet.co.uk.Models
+{
+    public class HomeIndex
+    {
+    }
+}
